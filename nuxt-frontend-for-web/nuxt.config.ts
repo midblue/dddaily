fcceaa222b299f5d54393d907157d105a85fef0b
Jasper Stephenson <jasperstephenson@gmail.com>
2024-05-06 10:12:03 +0900

    url

--- nuxt-frontend-for-web/nuxt.config.ts
+++ nuxt-frontend-for-web/nuxt.config.ts
@@ -12,7 +12,6 @@ export default defineNuxtConfig({
   },
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
-    baseURL: '/dddaily_frontend',
     head: {
       titleTemplate: (titleChunk) => {
         return 'DDDaily'
