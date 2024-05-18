941ea38110ca7503fc52e123389fd2b2f5a30688
Jasper Stephenson <jasperstephenson@gmail.com>
2024-05-18 13:12:44 +0900

    remove route

--- server/routes/remove.ts
+++ server/routes/remove.ts
@@ -8,13 +8,15 @@ const routes = Router()
 routes.post('/', async (req, res) => {
   const path = req.body.path as GettablePath
   if (!path || !Array.isArray(path) || path.length < 1) {
+    c.log('gray', 'Invalid path')
     res.status(401).json({ message: 'Invalid' })
     return
   }
   let userId = path[0].id
-  const rawPassword = req.headers.authorization as string
+  // const rawPassword = req.headers.authorization as string
 
-  if (!userId || !rawPassword) {
+  if (!userId) {
+    c.log('gray', 'Invalid userId or rawPassword')
     res.status(401).json({ message: 'Invalid' })
     return
   }
@@ -24,20 +26,22 @@ routes.post('/', async (req, res) => {
       `users/User-${userId}`,
     )
   if (!naiveUserData) {
+    c.log('gray', 'Invalid user')
     res.status(401).json({ message: 'Invalid' })
     return
   }
 
-  if (
-    !bcrypt.compareSync(
-      rawPassword,
-      naiveUserData.hashedPassword ||
-        '8929h30jrionu0vw9eijnfb20porqjwdnbwi9efsougbw3',
-    )
-  ) {
-    res.status(401).json({ message: 'Invalid' })
-    return
-  }
+  // if (
+  //   !bcrypt.compareSync(
+  //     rawPassword,
+  //     naiveUserData.hashedPassword ||
+  //       '8929h30jrionu0vw9eijnfb20porqjwdnbwi9efsougbw3',
+  //   )
+  // ) {
+  //   c.log('gray', 'Invalid password')
+  //   res.status(401).json({ message: 'Invalid' })
+  //   return
+  // }
 
   c.log('gray', 'Removing', path)
   await db.remove(path)
