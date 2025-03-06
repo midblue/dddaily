70f9bf03d4e377eacc41509449c1cbf918e03675
Jasper Stephenson <jasperstephenson@gmail.com>
2025-03-06 22:07:41 +0900

    a

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,7 +26,7 @@ export default defineNuxtConfig({
         {
           rel: 'icon',
           type: 'image/x-icon',
-          href: '/icon.ico',
+          href: '/dddaily/icon.ico',
         },
       ],
     },
